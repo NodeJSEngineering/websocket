7d73b5370ea9b0d9d12abb4131416092fb23a650
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-05-19 19:40:15 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# About
+
+npm install socket.io
+
+npm install --save-optional bufferutil utf-8-validate
+
+
+npm install express
+npm install --save-dev nodemon
+
+more info-
+https://tsh.io/blog/socket-io-tutorial-real-time-communication/
+https://socket.io/docs/v4/broadcasting-events/
+
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// // App setup
+const PORT = 5000;
+const app = express();
+const server = app.listen(PORT, function () {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+// Static files
+// app.use(express.static("public"));
+
+
+
+// Socket setup
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:4200",
+    methods: ["GET", "POST"]
+}})
+
+const activeUsers = new Set();
+io.on("connection",  (socket) => { 
+ console.log(socket.id, 'socket.id'); // x8WIv7-mJelg7on_ALbx // // server-side Each new connection is assigned a random 20-characters identifier.
+  console.log("Made socket connection");
+
+  socket.on("new user", function (data) {
+  console.log(data, 'new user FROM client');
+
+    socket.userId = data;
+    activeUsers.add(data);
+    io.emit("new user", [...activeUsers]);
+  });
+
+  socket.on("disconnect", () => {
+    
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
+  });
+
+  socket.on("chat message", function (data) {
+  console.log(data, 'chat FROM client');
+
+    io.emit("chat message", data);
+});
+
+socket.on("typing", function (data) {
+  console.log(data, 'typing FROM client');
+
+    socket.broadcast.emit("typing", data);
+  });
+
+  socket.on('join', function(data) {
+    console.log(data, 'join FROM client');
+    
+    // event emit name, t ext message
+    socket.emit('messages', 'Hello from server');
+ });
+
+
+});
